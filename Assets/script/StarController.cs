6463d6dab124ed0933bec63423886b5adaa6bd32
latifah221b <77819557+latifah221b@users.noreply.github.com>
2024-10-13 11:35:41 +0300

    updates ..

--- Assets/script/StarController.cs
+++ Assets/script/StarController.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collectable_logic : MonoBehaviour
+public class StarController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SelfDestory());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     IEnumerator  SelfDestory()
