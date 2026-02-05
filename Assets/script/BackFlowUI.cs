aa4a050f3602aa2d06a960b25252ab782f944b00
Saud Alnusairi <152731042+salnusairi@users.noreply.github.com>
2026-02-05 14:53:25 +0300

    new back button

---
+++ Assets/script/BackFlowUI.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class BackFlowUI : MonoBehaviour
+{
+    public GameObject backPanel;
+    public GameObject confirmBackPanel;
+
+    void Start()
+    {
+        backPanel.SetActive(false);
+        confirmBackPanel.SetActive(false);
+    }
+
+    
+    public void OpenBackPanel()
+    {
+        confirmBackPanel.SetActive(false);
+        backPanel.SetActive(true);
+    }
+
+    
+    public void OpenConfirmPanel()
+    {
+        backPanel.SetActive(false);
+        confirmBackPanel.SetActive(true);
+    }
+
+    public void ConfirmNo()
+    {
+        confirmBackPanel.SetActive(false);
+       
+    }
+}
