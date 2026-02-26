33fb94de77ce316647d74babeefac5bee5fb227f
Saud Alnusairi <152731042+salnusairi@users.noreply.github.com>
2026-02-26 11:51:13 +0300

    adding logic for clearing data

---
+++ Assets/script/ClearAllData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearAllData : MonoBehaviour
+{
+    
+    void Start()
+    {
+        AchievementSaveSystem.ClearAllAchievements();
+        InventorySaveSystem.ClearAllItems();
+        PlayerPrefs.DeleteAll();
+        Debug.Log("ALL DATA CLEARED!");
+    }
+    
+}
